f5dde523e56046cbc599934bd5e8259500b367fb
Mahmudur Rahman <mrforhad1@gmail.com>
2017-08-15 12:08:34 +0600

    Initial Commit

---
+++ Map Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Map Test
+//
+//  Created by Mahmudur Rahman on 8/15/17.
+//  Copyright © 2017 Code Mask. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
