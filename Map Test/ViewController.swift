2bcf59c6cf12c9719255b3fa984eda5e825e8fb2
Mahmudur Rahman <mrforhad1@gmail.com>
2017-08-15 13:04:10 +0600

    First Commit

--- Map Test/ViewController.swift
+++ Map Test/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var firstMap: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude: CLLocationDegrees = 23.7424828
+        let longitude: CLLocationDegrees = 90.3944231
+        
+        let latDelta: CLLocationDegrees = 0.15
+        let longDelta: CLLocationDegrees = 0.15
+        
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        firstMap.setRegion(region, animated: true)
+    
     }
 
     override func didReceiveMemoryWarning() {
